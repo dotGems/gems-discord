fbc412e0865b3652d250d23c64214dcc9da3be93
Denis Carriere <carriere.denis@gmail.com>
2022-01-04 14:06:19 -0500

    add account validation

--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
-import { client } from "./src/config";
+import { rpc, client } from "./src/config";
 
 client.on('ready', async listener => {
   console.log(`Logged in as ${listener.user.tag}!`);
 });
 
-client.on('messageCreate', async interaction => {
-  const pattern = interaction.content.match(/\$([a-z0-5\.]{1,12})/);
+async function is_account( account: string ) {
+  try {
+    await rpc.get_account(account);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
 
-  if ( pattern?.length ) {
-    const account = pattern[1];
+client.on('messageCreate', async interaction => {
+  for ( const word of interaction.content.split(" ")) {
+    const pattern = word[0];
+    const account = word.slice(1);
+    if ( account.length > 12 ) return;
+    if ( pattern != "$" ) return;
+    if ( !RegExp(/^[a-z1-5\.]+$/).test( account ) ) return;
+    if ( !(await is_account(account)) ) return;
     await interaction.reply(`🎉 Congrats! \`${account}\` was added to POP NFT snapshot 🥳`);
   }
 });
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,32 @@
+import { Api, JsonRpc } from 'eosjs';
+import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
+// import { Headers } from 'isomorphic-fetch';
+// import fetch from 'node-fetch';
+import fetch from 'isomorphic-fetch';
 import { Client, Intents } from 'discord.js';
-import dotenv from "dotenv"
+const { TextEncoder, TextDecoder } = require('util');
+// import dotenv from "dotenv";
+require("dotenv").config();
 
-dotenv.config();
+// REQUIRED configurations
+// if (!process.env.ACTOR) throw new Error("process.env.ACTOR is required");
+// if (!process.env.PRIVATE_KEYS) throw new Error("process.env.PRIVATE_KEYS is required");
+export const ACTOR = process.env.ACTOR;
+export const HEADERS = process.env.HEADERS;
+
+// // wrap fetch
+// function fetch(url: string, params = {} ) {
+//     if ( !HEADERS ) return require('node-fetch')( url, params );
+//     return require('node-fetch')(url, Object.assign(params, { headers: new Headers(JSON.parse(HEADERS)) }));
+// }
+
+// Discord
 if (!process.env.DISCORD_TOKEN) throw new Error("process.env.DISCORD_TOKEN is required");
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN
-
 export const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
 client.login(DISCORD_TOKEN);
+
+// EOSIO endpoints
+export const EOSIO_CHAIN_ID = process.env.EOSIO_CHAIN_ID || 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906';
+export const EOSIO_RPC = process.env.EOSIO_RPC || 'https://eos.greymass.com';
+export const rpc = new JsonRpc(EOSIO_RPC, {fetch});
