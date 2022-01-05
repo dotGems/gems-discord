96313c0f5b4177531527f581267e7fdbb594a2d0
Denis Carriere <carriere.denis@gmail.com>
2022-01-05 11:24:22 -0500

    update

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
 import { rpc, client } from "./src/config";
+import fs from "fs";
+import { Message } from "discord.js"
 
 client.on('ready', async listener => {
   console.log(`Logged in as ${listener.user.tag}!`);
 });
+
+// CONFIGURATIONS
+const CHANNEL_IDS = [
+  927337249203445900,
+]
+
+const exists = fs.existsSync("dicsord.csv");
+const writer = fs.createWriteStream("discord.csv", {flags: "a"});
+if ( !exists ) writer.write("discord,account,timestamp\n");
 
 async function is_account( account: string ) {
   try {
@@ -14,13 +25,28 @@ async function is_account( account: string ) {
 }
 
 client.on('messageCreate', async interaction => {
+  if ( CHANNEL_IDS.indexOf(Number(interaction.channelId)) == -1 ) return;
+
   for ( const word of interaction.content.split(" ")) {
     const pattern = word[0];
-    const account = word.slice(1);
-    if ( account.length > 12 ) return;
-    if ( pattern != "$" ) return;
-    if ( !RegExp(/^[a-z1-5\.]+$/).test( account ) ) return;
-    if ( !(await is_account(account)) ) return;
-    await interaction.reply(`🎉 Congrats! \`${account}\` was added to POP NFT snapshot 🥳`);
+    const message = word.slice(1);
+    if ( message.length > 12 ) continue;
+    if ( pattern != "$" ) continue;
+    await handle_message(interaction, pattern, message );
   }
 });
+
+function handle_message( interaction: Message<boolean>, pattern: string, message: string ) {
+  if ( pattern == "$" ) return register_account( interaction, message );
+}
+
+async function register_account( interaction: Message<boolean>, account: string ) {
+  if ( !valid_account( account ) ) return;;
+  if ( !(await is_account(account)) ) return;
+  await interaction.reply(`🎉 Congrats! \`${account}\` was added to POP NFT snapshot 🥳`);
+  writer.write([interaction.member, account, interaction.createdAt].join(","));
+}
+
+function valid_account( account: string ) {
+  return RegExp(/^[a-z1-5\.]+$/).test( account );
+}
